bab17ed52ca1ebe8217e478aaf5b4125f1f09957
ThisIsAimLegend <aimlegend.yt@gmail.com>
2021-08-01 14:34:36 +0200

    Combined all abilities of the bot extended comment functions improved log
    Co-authored-by: derTrichter <derTrichter@users.noreply.github.com>

--- insta_bot.py
+++ insta_bot.py
@@ -11,18 +11,18 @@ import datetime as dt
 
 import actions.excel_actions as excel
 import actions.comment_actions as comments
-
+import actions.structural_actions as structure
 
 #closes the picture
 def goBack():
-    #print("Go back")
-    keyboard.press_and_release("esc")
+    close_button = driver.find_elements_by_class_name("wpO6b")
+    close_button[-1].click()
 
 #likes every picture until there is no right arrow
-def like_pictures():
-    while True:
+def like_pictures(likes):
+    for i in range(likes):
         act = ActionChains(driver)
-        like = driver.find_element_by_class_name("ZyFrc")
+        like = WebDriverWait(driver, 7).until(EC.presence_of_element_located((By.CLASS_NAME,'ZyFrc')))
         act.double_click(like).perform()
         time.sleep(0.5)
         try:
@@ -45,18 +45,32 @@ def ClickOnStory():
             break
         time.sleep(1)
 
+def ClickOnPicture():
+    driver.find_element_by_class_name("eLAPa").click()
+    time.sleep(1)
+
 
 #NOT READY!!!
 #sends a follow request to the targetted account
 def following():
     keyboard.press_and_release("f5")
     time.sleep(3)
-    keyboard.press_and_release("tab")
-    time.sleep(0.1)
+    #keyboard.press_and_release("tab")
+    #time.sleep(0.1)
     keyboard.press_and_release("tab")
     time.sleep(0.1)
     keyboard.press_and_release("enter")
     time.sleep(0.5)
+
+#check if already follows target account
+def check_if_follow():
+    try: 
+        driver.find_element_by_class_name("glyphsSpriteFriend_Follow")
+    except: 
+        following()
+    pass
+
+
 
 #signs up into the selected bot account
 def sign_up(name, pw):
@@ -64,27 +78,46 @@ def sign_up(name, pw):
     driver.find_element_by_name("password").send_keys(pw)
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "eGOV_"))).click()
 
+def find_comment():
+   tags = structure.getPostTags(driver)
+
 #posts random comment from topic list
-def send_comment(topic):
-    comment = comments.returnFullComment(topic)
-    if comment == None:
+def send_comment(topic,cpp):
+    comment_test = comments.returnFullComment(topic)
+    if comment_test == None:
         pass
     else:
-        time.sleep(1)
-        driver.find_element_by_css_selector('textarea[aria-label="Kommentar hinzufügen ..."]').click()
+        for i in range(cpp):
+            comment = comments.returnFullComment(topic)
+            time.sleep(1)
+            driver.find_element_by_css_selector('textarea[aria-label="Kommentar hinzufügen ..."]').click()
+            time.sleep(0.5)
+            keyboard.write(comment)
+            keyboard.press_and_release("Enter")
+            time.sleep(1)
+
+def comment_loop(topic,comment_count,cpp):
+    for i in range(comment_count):
+        send_comment(topic,cpp)
         time.sleep(0.5)
-        keyboard.write(comment)
-        keyboard.press_and_release("Enter")
+        try:
+            driver.find_element_by_class_name("coreSpriteRightPaginationArrow").click()
+        except:
+            print("Alle Kommentare abgeschickt")
+            break
         time.sleep(1)
 
 #creates the log as a list
-def create_log(target, bot):
+def create_log(target, bot, topic, like_count, comment_count):
     timer = dt.datetime.now()
     timer = timer.strftime("[%d.%m.%Y , %H:%M:%S]")
     log = []
     log.append(timer)
     log.append("Ziel: " + str(target))
     log.append("Bot: " + str(bot))
+    log.append("Pictures liked: " + str(like_count))
+    log.append("Comments posted: " + str(comment_count))
+    log.append("Comment topic: " + str(topic))
     return log
     
 #creates a log in "bot_log.txt"
@@ -139,33 +172,36 @@ def ClickOnAccount():
     time.sleep(2)
 
 #clicks on the latest picture on this account
-def botting_actions(target,bot,topic):
-    bot_memory = excel.SearchForAccount(target,bot)
-    if bot_memory == False:
-        try:
-            select_picture = driver.find_element_by_class_name("eLAPa")
-            select_picture = True
-        except:
-            select_picture = False
-        if select_picture == True:
-            ClickOnStory()
-            driver.find_element_by_class_name("eLAPa").click()
-            time.sleep(2)
-            log = create_log(target,bot)
-            logging(log)
-            send_comment(topic)
-            like_pictures()
-            time.sleep(1)
-            excel.AddTargetToMemory(target,bot)
+def botting_actions(target,bot,topic,likes,comment_count,cpp):
+    try:
+        select_picture = driver.find_element_by_class_name("eLAPa")
+        select_picture = True
+    except:
+        select_picture = False
+    if select_picture == True:
+        check_if_follow()
+        ClickOnStory()
+        log = create_log(target,bot, topic, likes, comment_count)
+        logging(log)
+        if likes == None:
+            pass
         else:
-            following()
-            time.sleep(2)
-            driver.quit()
-    elif bot_memory == True:
-        print("Bot wurde bereits für den Ziel-Account ausgeführt")
-        pass
+            ClickOnPicture()
+            like_pictures(likes)
+        time.sleep(0.5)
+        goBack()
+        time.sleep(0.5)
+        if comment_count == 0:
+            pass
+        else:
+            ClickOnPicture()
+            comment_loop(topic,comment_count,cpp)
+        time.sleep(1)
+        excel.AddTargetToMemory(target,bot)
     else:
-        print("Fehler beim Auslesen der Accountliste")
+        check_if_follow()
+        time.sleep(2)
+        driver.quit()
 
 #stops the program and closes the browser window
 def EndProgram(bot):
