6b484c4501d5f0059c1f1d7ac40de74883c9cea8
ThisIsAimLegend <aimlegend.yt@gmail.com>
2021-08-06 23:27:23 +0200

    improved GUI by derTrichter

--- GUI.py
+++ GUI.py
@@ -1,31 +1,62 @@
 from tkinter import *
-from typing import Container
+from typing import Container, Sized
+import time
+from sys import exit
+import test_file as tf
+from actions.structural_actions import chooseAccounts
+import insta_bot as ib
+from main import one
 
 root = Tk()
 
+
+#funtions in GUI
+def darkMode():
+    root["bg"]="#000000"
+    title["bg"]="#000000"
+    title["fg"]="#ffffff"
+    command1["bg"]="#000000"
+    command1["fg"]="#ffffff"
+    command2["bg"]="#000000"
+    command2["fg"]="#ffffff"
+    dark["bg"]="#000000"
+    dark["fg"]="#ffffff"
+    return
+
+def lightMode():
+    root["bg"]="#ffffff"
+    title["bg"]="#ffffff"
+    title["fg"]="#000000"
+    command1["bg"]="#ffffff"
+    command1["fg"]="#000000"
+    command2["bg"]="#ffffff"
+    command2["fg"]="#000000"
+    dark["bg"]="#ffffff"
+    dark["fg"]="#000000"
+    return
+
+#content of GUI
 root.title("Instagram Bot")
-root.geometry("400x300")
+title = Label(root, text="Bot Settings", )
+command1 = Label(root, text="target account")
+command2 = Label(root, text="number of bots")
+dark = Label(root, text="Dark Mode")
+on = Button(root, text="ON", command=darkMode)
+off = Button(root, text="OFF", command=lightMode)
+start = Button(root, text="START", command=one)
 
-title_frame = Frame(root, width="300",height=20, borderwidth=2,relief="sunken")
-title_frame.pack(anchor="center")
-#title_frame.grid(row=1,column=1,columnspan=2)
+#styling of GUI
+root.geometry("1100x800")
+title["font"]="Ariel", 20
 
-settings_frame = Frame(root, width="300", borderwidth=2,relief="ridge")
-settings_frame.pack(side="top")
-#settings_frame.grid(row=2,column=1,columnspan=2)
+#placing elements
+title.grid(row=0, column=0)
+command1.grid(row=1, column=0,)
+command2.grid(row=1, column=1)
+dark.grid(row=2, column=0)
+on.grid(row=2, column=1)
+off.grid(row=2, column=2)
+start.grid(row=3, column=0)
 
-
-
-title = Label(title_frame,text="Bot Einstellungen")
-title.grid(row=1,column=1,pady=2,padx=2)
-
-target_label = Label(settings_frame,text="Ziel Account: ", borderwidth=2,relief="ridge")
-target_label.grid(row=2,column=1)
-
-target_label = Label(settings_frame,text="Ziel Account: ", borderwidth=2,relief="ridge")
-target_label.grid(row=2,column=1)
-
-target_input = Entry(settings_frame)
-target_input.grid(row=2,column=2)
 
 root.mainloop()
