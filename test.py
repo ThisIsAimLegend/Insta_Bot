1bb31fcc5bcdf7e4910bdc43066851b729877d79
derTrichter <jakob.richter05@gmail.com>
2021-08-25 21:38:09 +0200

    password to my pc

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-import actions.main as main
-
-if __name__ == "__main__":
-    main.test(["fcbayern"],11,1,1,1,"Sport",None)
-
+import actions.comment_actions as ca
+import actions.test_file as tf
+import datetime as dt
+import actions.excel_actions as ea
+import actions.structural_actions as sa
+import actions.comments as comments
